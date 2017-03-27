c1724e773bcaa1954548774c217ede10e6aa5dfc
zhangmian <847712857@qq.com>
2017-03-27 20:20:05 +0800

    错误修改公告类添加

---
+++ SmartCity.Domain/Entities/News.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCity.Domain.Entities
+{
+    /// <summary>
+    /// 公告实体类
+    /// </summary>
+    public class News
+    {
+    }
+}
