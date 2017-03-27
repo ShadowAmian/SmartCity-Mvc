90577456611ded8c6835bc337bf7594f78f78abf
zhangmian <847712857@qq.com>
2017-03-27 22:02:16 +0800

    通知公告模块添加

--- SmartCity.Domain/Entities/News.cs
+++ SmartCity.Domain/Entities/News.cs
@@ -11,5 +11,50 @@ namespace SmartCity.Domain.Entities
     /// </summary>
     public class News
     {
+        /// <summary>
+        /// 公告编号
+        /// </summary>
+        public int NewsID { get; set; }
+        /// <summary>
+        /// 公告标题
+        /// </summary>
+        public string NewsTitle { get; set;  }
+        /// <summary>
+        /// 公告缩略标题
+        /// </summary>
+        public string NewsSimpleTitle { get; set; }
+        /// <summary>
+        /// 公告分类
+        /// </summary>
+        public string NewsCharssify { get; set; }
+        /// <summary>
+        /// 公告关键词
+        /// </summary>
+        public string NewsKaywords { get; set; }
+        /// <summary>
+        /// 公告摘要
+        /// </summary>
+        public string NewsDigest { get; set; }
+        /// <summary>
+        /// 公告作者
+        /// </summary>
+        public string Author { get; set; }
+        /// <summary>
+        /// 是否发布
+        /// </summary>
+        public int IsComment { get; set; }
+        /// <summary>
+        /// 新闻图片
+        /// </summary>
+        public string NewsImages { get; set; }
+        /// <summary>
+        /// 新闻内容
+        /// </summary>
+        public string NewsContent { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
     }
 }
---
+++ SmartCity.WebUI/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartCity.WebUI.Areas.Admin.Controllers
+{
+    /// <summary>
+    /// 公告控制器
+    /// </summary>
+    public class NewsController : Controller
+    {
+        // GET: Admin/News
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
