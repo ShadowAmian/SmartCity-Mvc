7667c445ae965d443e57e6133b2c9088639aabb2
张冕 <847712857@qq.com>
2017-05-05 16:03:40 +0800

    1

---
+++ SmartCity.WebUI/Controllers/HomeNewsController.cs
@@ -0,0 +1,63 @@
+﻿using SmartCity.Common;
+using SmartCity.Domain.Abstract;
+using SmartCity.Domain.Entities;
+using SmartCity.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartCity.WebUI.Controllers
+{
+    public class HomeNewsController : Controller
+    {
+        #region 字段 构造函数
+        /// <summary>
+        /// 日志记录
+        /// </summary>
+        private INoticeInfo NewsInfoService;
+        private IPostsInfo PostInfoService;
+        private IUserInfo UserInfoService;
+        private IReviewInfo ReviewInfoService;
+        public HomeNewsController(INoticeInfo NewsInfo, IPostsInfo PostInfo, IUserInfo UserInfos, IReviewInfo ReviewInfo)
+        {
+            this.NewsInfoService = NewsInfo;
+            this.PostInfoService = PostInfo;
+            this.UserInfoService = UserInfos;
+            this.ReviewInfoService = ReviewInfo;
+        }
+        #endregion
+        // GET: HomeNews
+        public ActionResult HomeNewsList()
+        {
+            var model = SessionHelper.GetSession("HomeUserInfo");
+            //获取通知公告
+            var Model = new HomeNewsModel();
+            var result = NewsInfoService.GetNewsListByPublished().ToList();
+            Model.NewsItems = result;
+            int PageCount = 0;
+            //获取热门帖子
+            var HotPostsModel = PostInfoService.GetHotPostsInfo().ToList();
+            //获取标签
+            var PostsType = PostInfoService.SerachPostsType().ToList();
+            //获取最新评论
+            var LatestReviews = ReviewInfoService.GetLatestReviews().ToList();
+            Model.HotPostsItems = HotPostsModel;
+            Model.PostsTypeItems = PostsType;
+            Model.LatestReviewsItems = LatestReviews;
+            Model.Title1 = "Hi, 请登录";
+            Model.Tiltle2 = "我要注册";
+            Model.TitleUrL1 = "#";
+            Model.TitleUrl2 = "#";
+            Model.PageCount = PageCount;
+            if (model != null)
+            {
+                var models = model as User;
+                Model.Title1 = "Hi, 欢迎你";
+                Model.Tiltle2 = models.UserName;
+            }
+            return View(Model);
+        }
+    }
+}
---
+++ SmartCity.WebUI/Models/HomeNewsModel.cs
@@ -0,0 +1,38 @@
+﻿using SmartCity.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartCity.WebUI.Models
+{
+    public class HomeNewsModel
+    {
+
+        /// <summary>
+        /// 公告类
+        /// </summary>
+        public List<Notice> NewsItems { get; set; }
+        /// <summary>
+        /// 热门论坛类
+        /// </summary>
+        public List<Posts> HotPostsItems { get; set; }
+        /// <summary>
+        /// 最新评论
+        /// </summary>
+        public List<Review> LatestReviewsItems { get; set; }
+        /// <summary>
+        /// 标签分布
+        /// </summary>
+        public List<PostsType> PostsTypeItems { get; set; }
+        public string Title1 { get; set; }
+        public string TitleUrL1 { get; set; }
+        public string Tiltle2 { get; set; }
+
+        public string TitleUrl2 { get; set; }
+        /// <summary>
+        /// 记录总条数
+        /// </summary>
+        public int PageCount { get; set; }
+    }
+}
